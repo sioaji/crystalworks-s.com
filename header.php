0f7ba0eaa7cc5d89fbbfe06deffd6c69f452ef34
kuni <sioaji2000@yahoo.co.jp>
2025-09-21 15:32:03 +0900

    TOPクリックでもホームへ戻る

--- header.php
+++ header.php
@@ -1,8 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a href="index.php">
+    <a href="index.php" class="navbar-brand">
     <img src="./photo-1/nav.png" width="28" height="28" alt="">
-  </a>
-  <strong> 神霊導師・鈴木照玉 top</strong>
+    </a>
+    <a href="index.php" class="navbar-brand">
+    <strong>神霊導師・鈴木照玉 top</strong>
+    </a>
   <button class="navbar-toggler menu-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
     aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="メニューを開く">
     <i class="fas fa-bars"></i> ご案内
