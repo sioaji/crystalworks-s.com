b44548a6bb1442f19bc7027628db5870aa9307cb
kuni <sioaji2000@yahoo.co.jp>
2025-09-21 23:06:05 +0900

    表示後は、リンクボタン点滅に。

--- js/image-switch.js
+++ js/image-switch.js
@@ -1,10 +1,21 @@
 window.addEventListener('load', () => {
-setTimeout(() => {
-    document.querySelectorAll('.link-block').forEach(block => {
-    const img1 = block.querySelector('.img1');
-    const img2 = block.querySelector('.img2');
-    img1.style.opacity = '0';
-    img2.style.opacity = '1';
+    const blocks = document.querySelectorAll('.link-block');
+
+    blocks.forEach(block => {
+        const img1 = block.querySelector('.img1');
+        const img2 = block.querySelector('.img2');
+        let showingFirst = true;
+
+        // ƒ‹[ƒvØ‚è‘Ö‚¦ŠÖ”
+        setInterval(() => {
+        if (showingFirst) {
+            img1.style.opacity = '0';
+            img2.style.opacity = '1';
+        } else {
+            img1.style.opacity = '1';
+            img2.style.opacity = '0';
+        }
+        showingFirst = !showingFirst;
+        }, 5000); // 5•b‚²‚Æ‚ÉØ‚è‘Ö‚¦
     });
-}, 3000); // 3•bŒã‚ÉØ‚è‘Ö‚¦
 });
