2b91789b6cdefb8dc6410253f3cba32d43eef8f1
kuni <sioaji2000@yahoo.co.jp>
2025-09-28 07:44:24 +0900

    headを共通化

--- girei.php
+++ girei.php
@@ -1,24 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
   <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>鈴木照玉top</title>
-    <!-- font -->
-    <link href="https://fonts.googleapis.com/css2?family=Yuji+Syuku&display=swap" rel="stylesheet">
-    <!-- Bootstrap 5.3.2 CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- font awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    <!-- my style -->
-    <link href="css/my_style.css" rel="stylesheet" type="text/css">
-    <style>
-    body{
-    background-color: #000000;
-    }
-    </style>
+    <?php include 'head.php'; ?>
   </head>
 
   <body>
---
+++ head.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="Keywords" content="鈴木照玉、霊能力、霊能者、悩み、霊視、ヒーリング、スピリチュアル">
+    <meta name="Description" content="主護神様より授けられた三つの神通力で心を癒します。霊といかに生き、いかに過ごすかが大切です。">
+    <meta name="google-site-verification" content="ow4J0pZTA8GONuzDUsolBAL-Jm96yui9uGROiluWgRs" />
+    <title>神霊導師・鈴木照玉公式サイト</title>
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css2?family=Yuji+Syuku&display=swap" rel="stylesheet">
+    <!-- Bootstrap 5.3.2 CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <!-- my style -->
+    <link href="css/my_style.css" rel="stylesheet" type="text/css">
+    <style>
+      body{
+        background-color: #000000;
+      }
+    </style>
+</head>
