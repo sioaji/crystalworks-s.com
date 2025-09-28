bb2a1711abe307506696d57929ccc989870d9ab5
kuni <sioaji2000@yahoo.co.jp>
2025-09-28 10:41:11 +0900

    相談ページもカスタマイズ

--- soudan.php
+++ soudan.php
@@ -22,10 +22,15 @@
 
     <div class="container-fluid">
       <div class="soudan">
-        <h1 class="text-center"><strong>主護神様より授けられた三つの神通力</strong></h1>
-        <h1 class="text-center"><strong>神通視力（霊視・透視、遠方を瞬間的に見る力）</strong></h1>
-        <h1 class="text-center"><strong>神通聴力（主護神様の声）</strong></h1>
-        <h1 class="text-center"><strong>神言力（主護神様の降霊）</strong></h1>
+        <h2 class="text-center"><strong>主護神様より授けられた三つの神通力</strong></h2>
+        <div class="washi-frame">
+          <h3><strong>神通視力</strong></h3>
+          <div>霊視・透視、遠方を瞬間的に見る力</div>
+          <h3><strong>神通聴力</strong></h3>
+          <div>主護神様の声</div>
+          <h3><strong>神言力</strong></h3>
+          <div>主護神様の降霊</div>
+        </div>
         <p class="text-center">
         <strong>
         ◇　　◇　　◇<br>
@@ -52,70 +57,125 @@
 
     <div class="container-fluid">	  
       <div class="info">
-        <h5 class="sub-midashi col-lg-8 offset-lg-2"><strong>◎ご相談料</strong></h5>
-        <p class="col-lg-8 offset-lg-2">
-          <strong>
-          ◇基本料金…50,000円<br>
-          ・初回（2時間）※じっくりお話を聞いてから、鑑定いたします。&nbsp;<br>
-          ・2回目以降（リピーター価格）…30,000円&nbsp;&nbsp;
-          </strong>
-        </p>
-        <p class="col-lg-8 offset-lg-2">
-          <strong>
-          ◇神様降霊…50,000円 / 1回<br>
-          ※&nbsp; 希望者のみ事前予約が必要です。
-          </strong>
-        </p>
-        <p class="col-lg-8 offset-lg-2">
-          <strong>
-          ◇パワーヒーリング…10,000円 / 1回<br>
-          ※治癒力を高めます。  
-          </strong>
-        </p>
-        <p class="col-lg-8 offset-lg-2">
-          <strong>
-          ◇遠隔ヒーリング…5,000円 / 1回 <br>
-          ※5日間からになります。
-          </strong>
-        </p>
-        <p class="col-lg-8 offset-lg-2">
-          <strong>
-          ◇丑三つ祈祷料…30,000円 / 1回 <br>
-          ※大きな問題解決が必要な場合のみ。（回数は神様降霊にてお伝えします）<br>
-          </strong>
-        </p>
-        <p class="col-lg-8 offset-lg-2">
-          <strong>
-          ◇土地、家屋のお清めなどは、別途ご相談ください。
-          </strong>
-        </p>
-        <p class="col-lg-8 offset-lg-2">&nbsp;</p>
+        <div class="topic-frame">
+          <h5 class="sub-midashi"><strong><i class="fa-solid fa-eye custom-icon-topic"></i> ご相談料</strong></h5>
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <div class="autobio">
+                <table class="profile-table" style="width:100%;margin:auto;border-collapse:separate;border-spacing:0 8px;">
+                  <tbody>
+                    <tr>
+                      <td class="profile-date">基本料金</td>
+                      <td>50,000円：初回（2時間）<br>
+                      ※じっくりお話を聞いてから、鑑定いたします。<br>
+                      30,000円：2回目以降（リピーター価格）</td>
+                    </tr>
+                    <tr>
+                      <td class="profile-date">神様降霊</td>
+                      <td>50,000円 / 1回<br>
+                      ※希望者のみ事前予約が必要です。</td>
+                    </tr>
+                    <tr>
+                      <td class="profile-date">パワーヒーリング</td>
+                      <td>10,000円 / 1回<br>
+                      ※治癒力を高めます。</td>
+                    </tr>
+                    <tr>
+                      <td class="profile-date">遠隔ヒーリング</td>
+                      <td>5,000円 / 1回<br>
+                      ※5日間からになります。</td>
+                    </tr>
+                    <tr>
+                      <td class="profile-date">丑三つ祈祷料</td>
+                      <td>30,000円 / 1回<br>
+                      ※大きな問題解決が必要な場合のみ。<br>
+                      （回数は神様降霊にてお伝えします）</td>
+                    </tr>
+                    <tr>
+                      <td class="profile-date">土地・家屋のお清め等</td>
+                      <td>別途ご相談ください。</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="info">
+        <div class="topic-frame">
+          <h5 class="sub-midashi"><strong><i class="fa-solid fa-eye custom-icon-topic"></i> ご相談について</strong></h5>
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <div class="autobio">
+                <table class="profile-table" style="width:100%;margin:auto;border-collapse:separate;border-spacing:0 8px;">
+                  <tbody>
+                    <tr>
+                      <td class="profile-date">メール形式</td>
+                      <td>下記の形式にてメールでお問い合わせください。</td>
+                    </tr>
+                    <tr>
+                      <td class="profile-date">①お名前・ご年齢</td>
+                      <td>必須項目です。</td>
+                    </tr>
+                    <tr>
+                      <td class="profile-date">②ご住所・お電話番号</td>
+                      <td>必須項目です。</td>
+                    </tr>
+                    <tr>
+                      <td class="profile-date">③ご相談内容</td>
+                      <td>簡単にご相談内容などをお書きください。<br>※基本、照玉神霊道場（岐阜県大垣市）または東京相談所（台東区）での面談形式ですが、難しい場合は、その旨、申込時にお知らせください。<br>※出張などの特殊な場合を除き、メールのみで日時を決定いたします。<br>※スケジュール調整の関係で、返信が数日遅れることがあります。ご容赦ください。</td>
+                    </tr>
+                    <tr>
+                      <td class="profile-date">連絡先</td>
+                      <td>有限会社クリスタルワーク<br><a href="mailto:soudan@crystalworks-s.com">soudan@crystalworks-s.com</a></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div class="info">
-        <h5 class="sub-midashi col-lg-8 offset-lg-2"><strong>◎ご相談について</strong></h5>
-        <p class="col-lg-8 offset-lg-2"><strong>・下記の形式にてメールでお問い合わせください。</strong></p>
-        <p class="col-lg-8 offset-lg-2"><strong>①お名前・ご年齢</strong></p>
-        <p class="col-lg-8 offset-lg-2"><strong>②ご住所・お電話番号</strong></p>
-        <p class="col-lg-8 offset-lg-2"><strong>③簡単にご相談内容などをお書きください。<br>
-          ※基本、照玉神霊道場（岐阜県大垣市）または東京相談所（台東区）での面談形式ですが、難しい場合は、その旨、申込時にお知らせください。<br>
-          ※出張などの特殊な場合を除き、メールのみで日時を決定いたします。<br>
-          ※スケジュール調整の関係で、返信が数日遅れることがあります。ご容赦ください。
-        </strong></p>
-        <p class="col-lg-8 offset-lg-2"><strong>有限会社クリスタルワーク<br></strong></p>
-        <p class="col-lg-8 offset-lg-2"><a href="mailto:soudan@crystalworks-s.com  "> soudan@crystalworks-s.com</a>&nbsp;</p>
-        <p class="col-lg-8 offset-lg-2">&nbsp;</p>
-      </div>
-
-      <div class="info">
-        <h5 class="sub-midashi col-lg-8 offset-lg-2"><strong>◎出演依頼等について</strong></h5>
-        <p class="col-lg-8 offset-lg-2"><strong>・テレビ、Youtubeなど映像作品の出演、イベントなどの参加依頼など、まずは下記の形式にてメールでお問い合わせください。</strong></p>
-        <p class="col-lg-8 offset-lg-2"><strong>①会社名・団体名及び担当者様</strong></p>
-        <p class="col-lg-8 offset-lg-2"><strong>②日時</strong></p>
-        <p class="col-lg-8 offset-lg-2"><strong>③ご予算</strong></p>
-        <p class="col-lg-8 offset-lg-2"><strong>④ご依頼内容など</strong></p>
-        <p class="col-lg-8 offset-lg-2"><strong>有限会社クリスタルワーク<br></strong></p>
-        <p class="col-lg-8 offset-lg-2"><a href="mailto:info@crystalworks-s.com">info@crystalworks-s.com</a></p>
+        <div class="topic-frame">
+          <h5 class="sub-midashi"><strong><i class="fa-solid fa-eye custom-icon-topic"></i> 出演依頼等について</strong></h5>
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <div class="autobio">
+                <table class="profile-table" style="width:100%;margin:auto;border-collapse:separate;border-spacing:0 8px;">
+                  <tbody>
+                    <tr>
+                      <td class="profile-date">メール形式</td>
+                      <td>テレビ、Youtubeなど映像作品の出演、イベントなどの参加依頼など、まずは下記の形式にてメールでお問い合わせください。</td>
+                    </tr>
+                    <tr>
+                      <td class="profile-date">①会社名・団体名及び担当者様</td>
+                      <td>必須項目です。</td>
+                    </tr>
+                    <tr>
+                      <td class="profile-date">②日時</td>
+                      <td>必須項目です。</td>
+                    </tr>
+                    <tr>
+                      <td class="profile-date">③ご予算</td>
+                      <td>必須項目です。</td>
+                    </tr>
+                    <tr>
+                      <td class="profile-date">④ご依頼内容など</td>
+                      <td>必須項目です。</td>
+                    </tr>
+                    <tr>
+                      <td class="profile-date">連絡先</td>
+                      <td>有限会社クリスタルワーク<br><a href="mailto:info@crystalworks-s.com">info@crystalworks-s.com</a></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
